9499017e240d1bacac92510d797cec97ebd95c6d
DiogoPacta <diogo@rpfinconsult.com>
2025-06-24 15:26:39 -0300

    Refactor: Retorna à configuração padrão do Vite e PostCSS

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwind from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
+// Esta é a configuração padrão do Vite. Ele encontrará e usará 
+// automaticamente o postcss.config.js.
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        // Passamos o ficheiro de configuração explicitamente aqui
-        tailwind('./tailwind.config.js'),
-        autoprefixer,
-      ],
-    },
-  },
 })
